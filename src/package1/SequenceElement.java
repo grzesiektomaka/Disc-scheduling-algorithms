80df2032fb4703259677e730e2a17a8090444f42
Grzegorz <grzesiektomaka@gmail.com>
2019-04-03 01:22:36 +0200

    Add SequenenceElement class

---
+++ src/package1/SequenceElement.java
@@ -0,0 +1,27 @@
+package package1;
+
+public class SequenceElement {
+
+    private int position;
+    private boolean used_info = false;
+
+    public int getPosition() {
+
+        return position;
+    }
+
+    public boolean getUsedInfo() {
+        
+        return used_info;
+    }
+
+    public void setPosition(int position) {
+
+        this.position = position;
+    }
+
+    public void setUsedInfo(boolean used_info){
+
+        this.used_info = used_info;
+    }
+}
